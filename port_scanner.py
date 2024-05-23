202d5f32174fd4bba1934fb75ae2744b133776c1
Ravi Ranjan <74499665+kalyugera@users.noreply.github.com>
2024-05-23 23:42:47 +0530

    Add files via upload

---
+++ port_scanner.py
@@ -0,0 +1,22 @@
+import re
+from scapy.all import *
+
+try:
+    host = input("Enter a host address: ")
+    p = list(input("Enter the ports to scan (comma-separated): ").split(","))
+    ports = list(map(int, p))
+
+    if re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", host):
+        print("\n\nScanning...")
+        print("Host: ", host)
+        print("Ports: ", ports)
+
+        ans, unans = sr(IP(dst=host)/TCP(dport=ports, flags="S"), verbose=0, timeout=2)
+
+        for (s, r) in ans:
+            if r.haslayer(TCP) and r[TCP].flags == "SA":
+                print("[+] Port {} is open".format(s[TCP].dport))
+
+except (ValueError, RuntimeError, TypeError, NameError) as e:
+    print("[-] An error occurred:", e)
+    print("[-] Exiting...")
